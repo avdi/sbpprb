47561a1ca25e265e0f370cff032e091239f619d0
Avdi Grimm <avdi@avdi.org>
2011-09-24 23:59:15 -0400

    Complete Method Comment translation.

--- 14_method_comment.rb
+++ 14_method_comment.rb
@@ -1,3 +1,24 @@
 # Method Comment
 
-# Comments? We don't need no stinkin' comments! ;-)
+# Listing 1
+if (flags & 0b1000) == 1        # Am I visible?
+  # ...
+end
+
+# Listing 2
+def visible?
+  (flags & 0b1000) == 1
+end
+
+# Listing 3
+if visible?
+  # ...
+end
+
+# Listing 4
+class Bin
+  def run
+    # Tell my station to process me
+    station.process(self)
+  end
+end
