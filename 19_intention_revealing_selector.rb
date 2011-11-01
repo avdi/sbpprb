3f5e3e9e2e1de5482af9c479fff74250f4744869
Avdi Grimm <avdi@avdi.org>
2011-11-01 01:52:16 -0400

    Add Intention Revealing Selector

---
+++ 19_intention_revealing_selector.rb
@@ -0,0 +1,35 @@
+# Intention Revealing Selector
+
+# Listing 1
+class Array
+  def linear_search_for(value)
+    # ...
+  end
+end
+
+class Set
+  def hashed_search_for(value)
+    # ...
+  end
+end
+
+class BTree
+  def tree_search_for(value)
+    # ...
+  end
+end
+
+# Listing 2
+class Collection
+  def search_for(value)
+    # ...
+  end
+end
+
+# Listing 3
+class Collection
+  def include?(value)
+    # ...
+  end
+end
+
